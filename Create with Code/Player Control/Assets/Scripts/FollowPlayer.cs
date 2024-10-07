b732b49617124c2e48c63067dae6f4e3fadaaf11
Dany <71641218+DanyilT@users.noreply.github.com>
2024-10-07 19:07:18 +0100

    Unity Learn: Player Control (Create with Code Course)

--- Create with Code/Player Control/Assets/Scripts/FollowPlayer.cs
+++ Create with Code/Player Control/Assets/Scripts/FollowPlayer.cs
@@ -2,14 +2,8 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    public GameObject player;
-    private Vector3 offset = new Vector3(0, 10, -15);
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] public GameObject player;
+    [SerializeField] private Vector3 offset = new Vector3(0, 10, -15);
 
     // Update is called once per frame
     void LateUpdate()
--- Create with Code/Player Control/Assets/Scripts/PlayerController.cs
+++ Create with Code/Player Control/Assets/Scripts/PlayerController.cs
@@ -1,20 +1,75 @@
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 30f;
-    public float turnSpeed = 300f;
+    [SerializeField] private float horsePower = 1000;
+    [SerializeField] private float turnSpeed = 100f;
+    private float speed;
+    private float rpm;
+    private float horizontalInput;
+    private float verticalInput;
+    private Rigidbody playerRb;
+
+    [SerializeField] private GameObject centerOfMass;
+    [SerializeField] private TextMeshProUGUI speedometerText;
+    [SerializeField] private TextMeshProUGUI rpmText;
+
+    [SerializeField] private List<WheelCollider> allWheels;
+    private int wheelsOnGround;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.centerOfMass = centerOfMass.transform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * Input.GetAxis("Vertical"));
-        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * Input.GetAxis("Horizontal"));
+        // Get player input from player
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        // Check if the vehicle is on the ground
+        if (IsOnGround())
+        {
+            // Move the vehicle forward
+            //transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+            playerRb.AddRelativeForce(Vector3.forward * horsePower * verticalInput);
+
+            // Turning the vehicle
+            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
+
+            // Print the speed
+            speed = Mathf.Round(playerRb.linearVelocity.magnitude * 2.237f); // 3.6f for km/h
+            speedometerText.SetText("Speed: " + speed + " mph");
+
+            // Print the RPM
+            rpm = Mathf.Round((speed % 30) * 40);
+            rpmText.SetText("RPM: " + rpm);
+        }
+    }
+
+    private bool IsOnGround()
+    {
+        wheelsOnGround = 0;
+        foreach (WheelCollider wheel in allWheels)
+        {
+            if (wheel.isGrounded)
+            {
+                wheelsOnGround++;
+            }
+        }
+        if (wheelsOnGround == 4)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
