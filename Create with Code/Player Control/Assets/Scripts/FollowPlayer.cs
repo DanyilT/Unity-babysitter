2ca9971ab37697862463c24dd49368ea6f1aba63
Dany <71641218+DanyilT@users.noreply.github.com>
2024-10-08 06:10:49 +0100

    Unity Learn: Player Control (Create with Code Course)

--- Create with Code/Player Control/Assets/Scripts/FollowPlayer.cs
+++ Create with Code/Player Control/Assets/Scripts/FollowPlayer.cs
@@ -2,12 +2,47 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
+    [SerializeField] private Camera MainCamera;
+    [SerializeField] private Camera SecondCamera;
     [SerializeField] private GameObject player;
-    [SerializeField] private Vector3 offset = new Vector3(0, 10, -15);
+    [SerializeField] private Vector3 MainCameraOffset = new Vector3(0, 10, -15);
+    [SerializeField] private Vector3 SecondCameraOffset = new Vector3(0, 2, 1);
+    private bool fPressed = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        MainCamera.enabled = true;
+        SecondCamera.enabled = false;
+    }
 
     // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (fPressed)
+            {
+                MainCamera.enabled = true;
+                SecondCamera.enabled = false;
+                MainCamera.transform.position = player.transform.position + MainCameraOffset;
+            }
+            else
+            {
+                MainCamera.enabled = false;
+                SecondCamera.enabled = true;
+                SecondCamera.transform.position = player.transform.position + SecondCameraOffset;
+            }
+
+            fPressed = !fPressed;
+        }
+    }
+
+    // LateUpdate is called once per frame after Update
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        // Move the cameras to follow the player
+        MainCamera.transform.position = player.transform.position + MainCameraOffset;
+        SecondCamera.transform.position = player.transform.position + SecondCameraOffset;
     }
 }
